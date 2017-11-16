6a5aeb282c98c3a946a1446fc697aec820428271
unknown <superdada2@gmail.com>
2017-11-16 16:40:03 -0500

    V2.1

--- server/src/models/currency_enum.js
+++ server/src/models/currency_enum.js
@@ -1,6 +1,6 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define('currency_enum', {
     id: {
       type: DataTypes.INTEGER(11),
