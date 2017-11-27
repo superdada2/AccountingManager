f0fd5b1d0cbcacbf7a30e4a9b53a1e5d0ddb4f77
unknown <superdada2@gmail.com>
2017-11-27 15:50:18 -0500

    same

--- server/src/routes/api/reports/index.js
+++ server/src/routes/api/reports/index.js
@@ -13,7 +13,8 @@ import {
   GetIncomeDeferred,
   ModifyIncomeDeferred,
   loadData2,
-  GetHistory
+  GetHistory,
+  getDistinctUserName
 } from './controller'
 
 import {
@@ -22,7 +23,47 @@ import {
 
 export const router = express.Router()
 
+router.post('/getHistory', (req, res, next) => {
+  Authorize(req, res, next, [{
+    type: 1,
+    role: 2
+  }])
+}, async(req, res) => {
+  try {
+    console.log(req.body)
+    const result = await GetHistory(req.body)
+    res.status(200).json(result)
+  } catch (err) {
 
+    const message = err.message
+    console.log("error", message)
+    res.status(500).json({
+      status: false,
+      message
+    })
+  }
+})
+
+router.post('/getUsername', (req, res, next) => {
+  Authorize(req, res, next, [{
+    type: 1,
+    role: 2
+  }])
+}, async(req, res) => {
+  try {
+    console.log(req.body)
+    const result = await getDistinctUserName(req.body)
+    res.status(200).json(result)
+  } catch (err) {
+
+    const message = err.message
+    console.log("error", message)
+    res.status(500).json({
+      status: false,
+      message
+    })
+  }
+})
 router.post('/updateDescription', (req, res, next) => {
   Authorize(req, res, next, [{
     type: 1,
