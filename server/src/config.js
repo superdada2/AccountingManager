0b758be323b758b11ed133db7241dfc772bd8598
unknown <superdada2@gmail.com>
2017-12-05 14:59:54 -0500

    changed settings

--- server/src/config.js
+++ server/src/config.js
@@ -1,13 +1,13 @@
 export const config = {
-  db:{
-    database:"viziya_invoices3",
-    user:"viziya",
-    pass:'viziya2',
-    options:{
+  db: {
+    database: "viziya_invoices",
+    user: "viziya",
+    pass: 'viziya2',
+    options: {
       dialect: 'mysql',
-      host:'localhost',
-      define:{
-        timestamps:false
+      host: 'localhost',
+      define: {
+        timestamps: false
       }
     }
   }
--- server/src/models/change_history.js
+++ server/src/models/change_history.js
@@ -11,7 +11,7 @@ module.exports = function (sequelize, DataTypes) {
     timestamp: {
       type: DataTypes.DATE,
       allowNull: false,
-      defaultValue: sequelize.fn('now')
+      defaultValue: sequelize.fn('NOW')
     },
     user: {
       type: DataTypes.STRING,
