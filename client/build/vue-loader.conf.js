dafc767ee4e9fb3feac100f5a5896932c5219326
unknown <superdada2@gmail.com>
2017-12-06 14:04:25 -0500

    Added nginx config

--- client/build/vue-loader.conf.js
+++ client/build/vue-loader.conf.js
@@ -4,6 +4,9 @@ const config = require('../config')
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  loaders: {
+    js: 'babel-loader!eslint-loader'
+  },
   loaders: utils.cssLoaders({
     sourceMap: isProduction
       ? config.build.productionSourceMap
